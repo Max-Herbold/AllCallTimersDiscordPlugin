42f1c716582b6383c2b6523427d788760ed6c48a
Max Herbold <49804267+Max-Herbold@users.noreply.github.com>
2024-12-28 13:56:13 +1100

    Patch update (#36)
    * Update timerText.tsx
    
    * Update index.tsx

--- allCallTimers/timerText.tsx
+++ allCallTimers/timerText.tsx
@@ -4,16 +4,14 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-export function TimerText({ text }: Readonly<{ text: string; }>) {
-    return <p className="usernameFont__71dd5 username__73ce9" style={{
-        margin: 0,
+export function TimerText({ text, className }: Readonly<{ text: string; className: string; }>) {
+    return <div className={`timeCounter ${className}`} style={{
+        // this margin value doesn't change the default size of the user container
+        marginTop: -6,
         fontWeight: "bold",
         fontFamily: "monospace",
-        fontSize: 12,
-        position: "absolute",
-        bottom: -8,
-        left: 38,
-        padding: 2,
-        borderRadius: 3
-    }}>{text}</p>;
+        // good size that doesn't touch username
+        fontSize: 11,
+        position: "relative",
+    }}>{text}</div>;
 }
